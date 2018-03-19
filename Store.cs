48bb2e8c992675e6165e6a8fc29b6454912d0421
Hungry Banana <37053224+hungrybanana@users.noreply.github.com>
2018-03-19 10:27:52 +0300

    Add files via upload

---
+++ Store.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Store : MonoBehaviour {
+
+	public GameManager gm;
+	public Slider progressSlider;
+	public Text storeCountText;
+	public Text buyButtonText;
+
+	public int storeTimer;
+	public int profit;
+
+	private bool startTimer;
+	private float currentTimer;
+
+	public int baseStorePrice;
+	public int storeMultiplier;
+	public int storeCount;
+
+	// Use this for initialization
+	void Start () {
+		storeCountText.text = storeCount.ToString ();
+		buyButtonText.text = "Buy ($" + baseStorePrice.ToString () + ")";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (startTimer) {
+			currentTimer = currentTimer + Time.deltaTime;
+			progressSlider.value = currentTimer / storeTimer;
+			if (currentTimer > storeTimer) {
+				startTimer = false;
+				gm.addToBalance (profit * storeCount);
+				progressSlider.value = 0;
+			}
+		}
+	}
+
+	public void clickme() {
+		if (storeCount > 0) {
+			currentTimer = 0;
+			startTimer = true;
+		}
+	}
+
+	public void buyStore(){
+		if (gm.canBuy (baseStorePrice)) {
+			gm.addToBalance (-baseStorePrice);
+			storeCount++;
+			storeCountText.text = storeCount.ToString ();
+
+			baseStorePrice = baseStorePrice + (storeCount * storeMultiplier);
+			buyButtonText.text = "Buy ($" + baseStorePrice.ToString () + ")";
+		}
+	}
+
+}
