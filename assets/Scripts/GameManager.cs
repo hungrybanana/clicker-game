f89619fa802893cf5aee1cf88d267e90f00706c0
Hungry Banana <37053224+hungrybanana@users.noreply.github.com>
2018-03-21 08:11:19 +0300

    Add files via upload

---
+++ assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public Text currentBalance;
+	public int balance;
+
+	public GameObject managerPanel;
+
+
+	// Use this for initialization
+	void Start () {
+		currentBalance.text = "$" + balance.ToString ();
+	}
+	
+	public void addToBalance(int amount) {
+		balance += amount;
+		currentBalance.text = "$" + balance.ToString ();
+	}
+
+	public bool canBuy(int amount) {
+		if (amount <= balance)
+			return true;
+		else
+			return false;
+	}
+
+	public void showManagers() {
+		managerPanel.SetActive (true);
+	}
+
+	public void hideManagers() {
+		managerPanel.SetActive (false);
+	}
+}
